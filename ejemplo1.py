b5a3eaac74111eb80223d5cc6b8ea32c8253a9fc
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-12-06 16:21:52 -0600

    ejemplo 1

---
+++ ejemplo1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+Ejemplo de un modulo en posadev
+"""
+
+import math
+import os
+
+
+def main():
+    """
+    Comentario de la función
+    """
+    print("Pi es:", math.pi)
+    print(os.path.exists("/etc/"))  # para saber si estoy en linux
+
+
+if __name__ == "__main__":
+    main()
